29d0de80d4b12b6ab0755c34f00cd026aeb3a95b
SebastienOtten <78730474+SebastienOtten@users.noreply.github.com>
2021-05-04 15:44:08 +0200

    Add files via upload

---
+++ src/Filmlist.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="filmlist"><h1>Liste des films</h1>
+
+  <div v-for="film in filmlist" :key="film.nomdufilm"> 
+      <Film v-bind:film="film"  @event_delete="deleteFilm" @event_update="updateFilm"></Film>
+      
+  </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Film from './film.vue'
+export default {
+    name: 'Filmlist' ,
+    components:{Film},
+    data(){
+        return{
+            film:{
+                nomdufilm : "",
+                realisateur:"",
+                acteur:"",
+                synopsis:"",
+                note:0,
+                commentaire:""
+            },
+            filmlist:[],
+            url:"http://localhost:8000/api/film/"
+        }        
+    },
+    methods: {
+        get_filmlist(){
+              axios.get(this.url)
+              .then( (response)=>{
+                this.filmlist = response.data;
+                 console.log(this.filmlist);
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+        },
+        deleteFilm(nomdufilm){
+              axios.delete(this.url + nomdufilm)
+              .then( (response)=>{
+                 console.log(response.data);
+                 this.get_filmlist();
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+
+        },
+        updateFilm(film) {
+            axios.put(this.url + film.nomdufilm, film)
+            .then((response)=>{
+                console.log(response.data);
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+        }
+    },
+    mounted() {
+       this.get_filmlist() 
+    }
+}
+</script>
+
+
+
+
+
+
+<style>
+ #filmlist {
+    background-color:var(--second-color);
+    margin: 10px auto;
+    padding: 5px;
+    width: 500px;
+    border-radius: 10px;
+    
+}
+</style>
